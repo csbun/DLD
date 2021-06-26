a7c0789286ccf69c038e070a13e5afdd8c7a8cf0
Hans Chan <hanchai.chc@alibaba-inc.com>
2021-06-26 16:18:31 +0800

    feat: hide window shortcuts

--- app.js
+++ app.js
@@ -6,24 +6,35 @@ const IS_DEBUG = !!process.env.VSCODE_INSPECTOR_OPTIONS;
 
 const mb = menubar({
   browserWindow: {
-    width: IS_DEBUG ? 800 : 400,
-    height: IS_DEBUG ? 800 : 400,
+    title: 'ft - Fast Translator',
+    width: IS_DEBUG ? 800 : 300,
+    height: IS_DEBUG ? 800 : 300,
   },
+  windowPosition: 'trayRight',
   icon: nativeImage.createFromPath(path.resolve(__dirname, 'icon', 'iconTemplate.png')),
   showDockIcon: false,
 });
 
 mb.on('ready', () => {
   // console.log('app is ready');
-  // 快捷键打开翻译窗口
+  // 快捷键：打开翻译窗口
   globalShortcut.register('CommandOrControl+Y', () => {
     mb.showWindow();
   });
 });
 
 
-
 mb.on('after-create-window', () => {
+  // do not showDockIcon
+  mb.app.dock.hide();
+  // 快捷键：隐藏窗口
+  mb.window.webContents.on('before-input-event', (event, input) => {
+    if (input.key.toLowerCase() === 'escape') {
+      mb.hideWindow();
+      event.preventDefault()
+    }
+  })
+  // DEV Tools
   if (IS_DEBUG) {
     mb.window.webContents.openDevTools();
   }
